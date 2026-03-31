597d5d81dc9fede8d54411b25fbf5619e63ac666
dawtor <danil.golikov11@gmail.com>
2026-03-31 22:37:35 +0700

    the mask functions for the card and account have been added and a date conversion function has been added.

---
+++ src/widget.py
@@ -0,0 +1,49 @@
+def mask_account_card(data: str) -> str:
+    """Применяет маски для номера карты и счета"""
+    parts = data.split()
+
+    if not parts:
+        raise ValueError("Неверный формат данных")
+
+    if parts[0] == "Счет":
+        account_number = parts[-1]
+
+        if not account_number.isdigit():
+            raise ValueError("Номер счета должен содержать только цифры")
+
+        if len(account_number) != 20:
+            raise ValueError("Номер счета должен содержать 20 цифр")
+
+        masked_acc = f"{account_number[-4:]}"
+        return f"{parts[0]} {masked_acc}"
+
+    elif parts[0] == "Карта":
+        card_number = parts[-1]
+        card_name = " ".join(parts[:-1])
+
+        if not card_number.isdigit():
+            raise ValueError("Номер карты должен содержать только цифры")
+
+        if len(card_number) != 16:
+            raise ValueError("Номер карты должен содержать 16 цифр")
+
+        masked_card = (
+            f"{card_number[:4]} "
+            f"{card_number[4:6]}** "
+            f"**** "
+            f"{card_number[-4:]}"
+        )
+        return f"{card_name} {masked_card}"
+
+    else:
+        raise ValueError("Неверный формат данных")
+
+
+from datetime import datetime
+def get_date(date_str: str) -> str:
+    """
+    Преобразует строку с датой в формате
+    '2024-03-11T02:26:18.671407' в формат 'ДД.ММ.ГГГГ'.
+    """
+    dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+    return dt.strftime("%d.%m.%Y")
