809e48b985fc0ca6969a8daf4864408915e4519e
dawtor <danil.golikov11@gmail.com>
2026-04-05 23:02:28 +0700

    ddwidget.py

--- src/widget.py
+++ src/widget.py
@@ -1,49 +1,28 @@
-def mask_account_card(data: str) -> str:
-    """Применяет маски для номера карты и счета"""
-    parts = data.split()
 
-    if not parts:
-        raise ValueError("Неверный формат данных")
-
-    if parts[0] == "Счет":
-        account_number = parts[-1]
-
-        if not account_number.isdigit():
-            raise ValueError("Номер счета должен содержать только цифры")
-
-        if len(account_number) != 20:
-            raise ValueError("Номер счета должен содержать 20 цифр")
-
-        masked_acc = f"{account_number[-4:]}"
-        return f"{parts[0]} {masked_acc}"
-
-    elif parts[0] == "Карта":
-        card_number = parts[-1]
-        card_name = " ".join(parts[:-1])
-
-        if not card_number.isdigit():
-            raise ValueError("Номер карты должен содержать только цифры")
-
-        if len(card_number) != 16:
-            raise ValueError("Номер карты должен содержать 16 цифр")
-
-        masked_card = (
-            f"{card_number[:4]} "
-            f"{card_number[4:6]}** "
-            f"**** "
-            f"{card_number[-4:]}"
-        )
-        return f"{card_name} {masked_card}"
-
-    else:
-        raise ValueError("Неверный формат данных")
-
-
+from masks import get_mask_card_number
+from masks import get_mask_account
+import re
 from datetime import datetime
-def get_date(date_str: str) -> str:
+
+def mask_account_card(account_card: str) -> str:
     """
-    Преобразует строку с датой в формате
-    '2024-03-11T02:26:18.671407' в формат 'ДД.ММ.ГГГГ'.
+    Принимает один аргумент — строку, содержащую тип и номер карты или счета,
+     и возвращает строку с замаскированным номером.
     """
-    dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
-    return dt.strftime("%d.%m.%Y")
+
+    if "Счет" in account_card:
+        letters_count = "".join(re.findall(r"\D+", account_card))
+        numbers_count =  "".join(re.findall(r"\d+", account_card))
+        return f"{letters_count} {get_mask_account()}"
+    else:
+        letters_card = "".join(re.findall(r"\D+", account_card))
+        numbers_card = "".join(re.findall(r"\d+", account_card))
+        return f"{letters_card} {get_mask_card_number()}"
+
+
+def get_date(date: str) -> str:
+    """
+    Функция преобразует дату в формат 'DD.MM.YYYY
+    '"""
+    date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    return date.strftime("%d.%m.%Y")
